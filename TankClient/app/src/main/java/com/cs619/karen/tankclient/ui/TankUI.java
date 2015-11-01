1cdc8e3c66ad7bba71fbe5cb233a11229c84d42b
Cameron Devine <cwh39@wildcats.unh.edu>
2015-11-01 15:11:27 -0500

    First project upload.

---
+++ TankClient/app/src/main/java/com/cs619/karen/tankclient/ui/TankUI.java
@@ -0,0 +1,7 @@
+package com.cs619.karen.tankclient.ui;
+
+/**
+ * Created by cdevine on 10/30/2015.
+ */
+public class TankUI {
+}
---
+++ TankClient/app/src/main/java/com/cs619/karen/tankclient/util/WallService.java
@@ -0,0 +1,7 @@
+package com.cs619.karen.tankclient.util;
+
+/**
+ * Created by cdevine on 10/30/2015.
+ */
+public class WallService {
+}
