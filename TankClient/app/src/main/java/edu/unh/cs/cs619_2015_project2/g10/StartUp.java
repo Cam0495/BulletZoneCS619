64423e22def1c6bf9365470835499a92f55f8ee7
Cameron Devine <cwh39@wildcats.unh.edu>
2015-11-15 22:25:45 -0500

    Added GameController

--- TankClient/app/src/main/java/edu/unh/cs/cs619_2015_project2/g10/StartUp.java
+++ TankClient/app/src/main/java/edu/unh/cs/cs619_2015_project2/g10/StartUp.java
@@ -1,12 +1,8 @@
 package edu.unh.cs.cs619_2015_project2.g10;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
